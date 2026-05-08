dc348c74bcc6c9f5de415cd2d4620ac2fb10d29f
Trauringe Tag <humans.vlone@gmail.com>
2026-05-08 10:25:59 +0500

    Добавлен функ. в views и создан urls

---
+++ main/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'main'
+
+# Регистрация всех путей веб-сайта
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('catalog/', views.CatalogView.as_view(), name='catalog_all'),
+    path('catalog/<slug:category_slug>/', views.CatalogView.as_view(), name='catalog'),
+    path('product/<slug:slug>/', views.ProductDetailView.as_view(), name='product_detail'),
+]
