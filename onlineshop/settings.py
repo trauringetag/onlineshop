217b0907b927cd955f4ff8ae553757ae00339689
Trauringe Tag <humans.vlone@gmail.com>
2026-09-08 14:43:12 +0500

    Создал приложение Cart

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -54,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    'cart.middleware.CartMiddleware',
 ]
 
 ROOT_URLCONF = 'onlineshop.urls'
@@ -68,6 +70,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                
+                'cart.context_processors.cart_processor',
             ],
         },
     },
