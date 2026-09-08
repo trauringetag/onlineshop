e007aba1a20aff291b3ef95f26637ad939bc9403
Trauringe Tag <humans.vlone@gmail.com>
2026-09-08 18:51:23 +0500

    Создание приложения Users

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     
     'main',
     'cart',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -138,3 +139,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 SESSION_COOKIE_AGE = 86400 # 30 дней
 SESSION_SAVE_EVERY_REQUEST = True
+
+AUTH_USER_MODEL = 'users.CustomUser'
