6a0d4760f60b9c323e2dd32bf235314aa31393ca
Trauringe Tag <humans.vlone@gmail.com>
2026-09-11 12:33:48 +0500

    Исправление ошибок №2

--- users/urls.py
+++ users/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('edit-account-details/', views.edit_account_details, name='edit_account_details'),
     path('update-account-details/', views.update_account_details, name='update_account_details'),
     path('logout/', views.logout, name='logout'),
+    path('order-history/', views.order_history, name='order_history'),
+    path('order/<int:order_id>/', views.order_detail, name='order_detail'),
 ]
--- users/views.py
+++ users/views.py
@@ -9,6 +9,7 @@ from .forms import CustomUserCreationForm, CustomUserLoginForm, \
 from .models import CustomUser
 from django.contrib import messages
 from main.models import Product
+from orders.models import Order
 
 
 def register(request):
@@ -94,3 +95,13 @@ def logout_view(request):
     if request.headers.get('HX-Request'):
         return HttpResponse(headers={'HX-Redirect': reverse('main:index')})
     return redirect('main:index')
+
+@login_required
+def order_history(request):
+    orders = Order.objects.filter(user=request.user).order_by('-created_at')
+    return TemplateResponse(request, 'users/partials/order_history.html', {'orders': orders})
+
+@login_required
+def order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id, user=request.user)
+    return TemplateResponse(request, 'users/partials/order_detail.html', {'order': order})
